0df3aeb46832a5e1cd9e9a5bd7013e3f76ef80ac
Shadow <143490135+Shadowfrost4886@users.noreply.github.com>
2024-09-29 20:44:27 -0400

    Update contribute.js

--- src/prefix-commands/contribute.js
+++ src/prefix-commands/contribute.js
@@ -1,1 +1,18 @@
-
+module.exports = {
+    run(message, args) {
+     
+      const githubLink = "https://github.com/Shadowfrost4886/NexusBot"; 
+  
+      
+      message.channel.send(`Thank you for your interest in contributing to the bot! You can check out our GitHub repository here: ${githubLink}. 
+  
+  We welcome contributions such as:
+  - Reporting bugs
+  - Suggesting features
+  - Submitting pull requests
+  - Improving documentation
+  
+  Feel free to explore and help us make this bot even better!`);
+    },
+  };
+  
